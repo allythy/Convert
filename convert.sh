c5ad49ebcc96ed22ec47d4a2a6af9ac21acd71c1
allythy <allythyrenan@gmail.com>
2017-04-17 12:32:25 -0300

    update

---
+++ convert.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo "
+           |--------------------------------------------------|
+           |                      Menu                        |
+           |--------------------------------------------------|
+           | 1) Binário para decimal                          |
+           | 2) Texto para binário                            |
+           |--------------------------------------------------|
+"
+printf "Escolha uma opção: "
+read opcao
+
+case $opcao in
+  1) while [ "$inicio" != "n" ]
+  do
+      read -p "Digite o número em binário: " numero
+
+      printf "Seu número em decimal: "
+      echo 'ibase=2;'$numero | bc
+      echo ""
+
+    read -p "Deseja continuar[S/N]" inicio
+  done ;;
+
+  2) while [ "$inicio" != "n" ]
+  do
+      read -p "Digite o texto para converter em binário: " texto
+
+       echo ""
+       echo "$texto" | xxd -b
+
+       echo ""
+       read -p "Deseja continuar[S/N]" inicio
+  done ;;
+
+  *) echo "Só tem a opção 1 e 2, NÃO SABE LER POOOOOORRA" ;;
+
+esac
