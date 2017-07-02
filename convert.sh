121de8e8c3ce735e7779f6a83d81db1abab1a54c
allythy <allythyrenan@gmail.com>
2017-07-02 18:37:52 -0300

    ajeitando a indentação

--- convert.sh
+++ convert.sh
@@ -15,25 +15,20 @@ case $opcao in
   1) while [ "$inicio" != "n" ]
   do
       read -p "Digite o número em binário: " numero
-
-      printf "Seu número em decimal: "
-      echo 'ibase=2;'$numero | bc
-      echo ""
-
-    read -p "Deseja continuar[S/N]" inicio
+      echo -n "Seu número em decimal: " ; echo 'ibase=2;'$numero | bc
+      echo
+      read -p "Deseja continuar[S/N]" inicio
   done ;;
 
   2) while [ "$inicio" != "n" ]
   do
-      read -p "Digite o texto para converter em binário: " texto
-
-       echo ""
+       read -p "Digite o texto para converter em binário: " texto
+       echo
        echo "$texto" | xxd -b
-
-       echo ""
+       echo
        read -p "Deseja continuar[S/N]" inicio
   done ;;
 
-  *) echo "Só tem a opção 1 e 2, NÃO SABE LER POOOOOORRA" ;;
+  *) echo "Só tem a opção 1 e 2" ;;
 
 esac
